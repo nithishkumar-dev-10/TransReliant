d0c08cef468aac9c808b32f0817d063d60c1c67d
Nithish Kumar Saravanan <nithishkumar.dev10@gmail.com>
2026-04-21 22:36:55 +0530

    added the classifer model

--- backend/ml/ml_engine.py
+++ backend/ml/ml_engine.py
@@ -99,4 +99,49 @@ def build_preprocessor(label_cols,onehot_cols,numeric_cols):
     return preprocessor
     # only the blue print is made , no real preprocessing is done here , the preprocessor will be fitted in the training function and then used for both train and test data to avoid data leakage
 
+# training the classifer model 
+def train_classifier(tickect_df):
+
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
     
+    target=config["features"]["ticket"]["target"]
+
+    #removing the target coloumn from the dataset, keeping only the features for training
+    X=tickect_df.drop(coloumns=[target])
+
+    #fixing the target coloumn 
+    y=tickect_df[target]
+    
+    #we are calling the function we have defined before 
+    label_cols,onehot_cols,numeric_cols=get_ticket_column_types(X,target=target)
+
+    preprocessor=build_preprocessor(label_cols=label_cols,onehot_cols=onehot_cols,numeric_cols=numeric_cols)
+
+    #setting the exact pipeline for the model
+
+    pipeline=Pipeline(steps=[("preprocessor",preprocessor),("model",RandomForestClassifier(random_state=42))])
+
+    #test train split 
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)  
+
+    #doing the entire process of builiding the model,all process takes place here
+    pipeline.fit(X_train,y_train)
+    
+    #precditing the data 
+    y_pred=pipeline.predict(X_test)
+
+    #evaluating the model
+    f1=f1_score(y_test,y_pred)
+    acc=accuracy_score(y_test,y_pred)
+    auc=roc_auc_score(y_test,y_pred)
+
+    print(f"F1 Score : {f1}")
+    print(f"Accuracy : {acc}")
+    print(f"AUC Score : {auc}")
+
+
+
+
+
+
