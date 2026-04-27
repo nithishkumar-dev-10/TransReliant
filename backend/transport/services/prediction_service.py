8d2cb1bc002b066a7f970c55f111d1507e1f47fb
Nithish Kumar Saravanan <nithishkumar.dev10@gmail.com>
2026-04-27 11:35:22 +0530

    updated the  prediction_services acc to new schemas

--- backend/transport/services/prediction_service.py
+++ backend/transport/services/prediction_service.py
@@ -1,39 +1,41 @@
-from backend.transport.schemas.request import TicketInput, DelayInput
-from backend.transport.utils.feature_encoder import encode_ticket_input, encode_delay_input
-from backend.ml.prediction import load_config, load_model, predict_confirmation, predict_delay
+
+from backend.transport.schemas.request import UserInput
+from backend.transport.utils.resolver import resolve
+from backend.ml.prediction import predict_confirmation, predict_delay, load_model, load_config
 from backend.transport.core.reliability import calculate_reliability
 
-config=load_config()
+
+
+config                = load_config()
 classifier, regressor = load_model(config)
 
-async def run_prediction_service(ticket_input: TicketInput, delay_input: DelayInput):
-    # Step 1: Encode inputs
-    tickect_dict=encode_ticket_input(ticket_input)
-    delay_dict=encode_delay_input(delay_input)
 
-    #step2 : using the models
-    conf_result=predict_confirmation(classifier,tickect_dict)
-    delay_result=predict_delay(regressor,delay_dict)
+async def run_prediction_service(user: UserInput) -> dict:
 
-    conf_prob=conf_result["confirmation_probability"]
-    delay_min=delay_result["delay_minutes"]
-    source=delay_input.Source
-    destination=delay_input.Destination
 
-    #step_3 : calculate reliablity
-    reliability_result=calculate_reliability(
-        confirmation_probability=conf_prob,
-        delay_minutes=delay_min,
-        source=source,
-        destination=destination
+    ticket_dict, delay_dict = resolve(user)
+
+    
+    conf_result  = predict_confirmation(classifier, ticket_dict)
+    delay_result = predict_delay(regressor, delay_dict)
+
+    
+    conf_prob   = conf_result["confirmation_probability"]
+    delay_min   = delay_result["delay_minutes"]
+    source      = user.source_station
+    destination = user.destination_station
+
+    
+    reliability_result = calculate_reliability(
+        confirmation_probability = conf_prob,
+        delay_minutes            = delay_min,
+        source                   = source,
+        destination              = destination
     )
 
+    
     return {
-        "ticket":conf_result,
-        "delay":delay_result,
-        "reliability":reliability_result
+        "ticket":      conf_result,
+        "delay":       delay_result,
+        "reliability": reliability_result
     }
-
-
-
-
