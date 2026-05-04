6cec1b4df096ae29d1835773172bfc5251de0f80
Nithish Kumar Saravanan <nithishkumar.dev10@gmail.com>
2026-05-04 18:09:09 +0530

    structured the code of predict.py in routes

--- backend/api/routes/predict.py
+++ backend/api/routes/predict.py
@@ -12,16 +12,11 @@ router = APIRouter()
 @router.post("/predict")
 async def predict(request: PredictionRequest):
     try:
-        
         result = await run_prediction_service(request.user)
-
-        
         return result
 
     except ValueError as e:
-        
         raise HTTPException(status_code=404, detail=str(e))
 
     except Exception as e:
-       
         raise HTTPException(status_code=500, detail=str(e))
